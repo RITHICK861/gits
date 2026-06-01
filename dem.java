5654e5e15751293730de02a6b907d85e1b9bb416
RITHICK861 <kmrithick71@gmail.com>
2026-06-01 17:48:21 +0530

    Update dem.java

--- dem.java
+++ dem.java
@@ -6,5 +6,6 @@ public class dem {
         System.out.println("Hello, World!");
         System.out.println("Hello");
         System.out.println("Hi");
+        System.out.println("cr7");
     }
 }
